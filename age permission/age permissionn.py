21e58f5c4e14a19922efe46ce27c02eee1285ef0
fatemehkazemiii <fatemehkazemi200180@gmail.com>
2025-12-02 02:03:25 +0330

    Add files via upload

---
+++ age permission/age permissionn.py
@@ -0,0 +1,7 @@
+age = int(input("how old are you?"))
+if age>=18:
+    print("you have permission to enter")
+else :
+    print("you dont have permission to enter")
+
+    
---
+++ age permission/main.py
@@ -0,0 +1,6 @@
+age = int(input("how old are you?"))
+if age>=18:
+    print("you have permission to enter")
+else :
+    print(" you dont have permission to enter")
+
